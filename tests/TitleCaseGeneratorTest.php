7f3e4a36a1f8a059f39e2fff232d42dbf9b7d20e
Kyle Bulloch & Kyle Giard-Chase <student@epicodus.com>
2015-03-09 10:30:49 -0700

    Add functions testMakeTitleCaseOneWord and testMakeTitleCaseMultipleWords to TitleCaseGeneratorTest.php

--- tests/TitleCaseGeneratorTest.php
+++ tests/TitleCaseGeneratorTest.php
@@ -4,7 +4,31 @@
 
     class TitleCaseGeneratorTest extends PHPUnit_Framework_TestCase
     {
+        function testMakeTitleCaseOneWord()
+        {
+            //Arrange
+            $test_title_case_generator = new TitleCaseGenerator;
+            $input = "beowulf";
 
+            //Act
+            $result = $test_title_case_generator->makeTitleCase($input);
+
+            //Assert
+            $this->assertEquals("Beowulf", $result);
+        }
+
+        function test_make_title_case_multiple_words()
+        {
+            //Arrange
+            $test_title_case_generator = new TitleCaseGenerator;
+            $input = "the dark lord";
+
+            //Act
+            $result = $test_title_case_generator->makeTitleCase($input);
+
+            //Assert
+            $this->assertEquals("The Dark Lord", $result);
+        }
 
     }
 
